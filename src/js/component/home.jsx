de71851a08ed42f84dc262627e576f8f1b85906e
Justice Rivera <142175716+justicetrace@users.noreply.github.com>
2024-07-05 23:07:07 +0000

    simple challenging counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,44 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const SimpleCounter = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="counter-box text-center">
+			<div className="clock"><i class="fa-regular fa-clock"></i></div>
+			<div className="one">{props.digitOne}</div>
+			<div className="two">{props.digitTwo}</div>
+			<div className="three">{props.digitThree}</div>
+			<div className="four">{props.digitFour}</div>
+			<div className="five">{props.digitFive}</div>
+			<div className="six">{props.digitSix}</div>
 		</div>
 	);
 };
 
-export default Home;
+SimpleCounter.PropTypes = {
+	digitSix: PropTypes.number,
+	digitFive: PropTypes.number,
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
+};
+
+let counter = 0;
+setInterval(function() {
+	const four = Math.floor(counter/1000);
+	const three = Math.floor(counter/100);
+	const two = Math.floor(counter/10);
+	const one = Math.floor(counter/1);
+	counter++;
+	ReactDOM.render(
+		<SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />,
+		document.querySelector('#app')
+	)
+}, 1000);
+
+export default SimpleCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,51 @@ import ReactDOM from "react-dom/client";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+
+import PropTypes from "prop-types";
+
+//include images into your bundle
+//create your first component
+const SimpleCounter = (props) => {
+	return (
+		<div className="counter-box text-center">
+			<div className="clock"><i class="fa-regular fa-clock"></i></div>
+			<div className="one">{props.digitOne}</div>
+			<div className="two">{props.digitTwo}</div>
+			<div className="three">{props.digitThree}</div>
+			<div className="four">{props.digitFour}</div>
+			<div className="five">{props.digitFive}</div>
+			<div className="six">{props.digitSix}</div>
+		</div>
+	);
+};
+
+SimpleCounter.propTypes = {
+	digitSix: PropTypes.number,
+	digitFive: PropTypes.number,
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
+};
+
+let counter = 50;
+setInterval(function() {
+    const six = Math.floor(counter/100000).toString().split('').reverse()[0]
+    const five = Math.floor(counter/10000).toString().split('').reverse()[0]
+	const four = Math.floor(counter/1000).toString().split('').reverse()[0]
+	const three = Math.floor(counter/100).toString().split('').reverse()[0]
+	const two = Math.floor(counter/10).toString().split('').reverse()[0]
+	const one = Math.floor(counter/1).toString().split('').reverse()[0]
+	counter++;
+	ReactDOM.createRoot(
+		document.querySelector('#app')).render(
+        <SimpleCounter digitSix={one} digitFive={two} digitFour={three} digitThree={four} digitTwo={five} digitOne={six}/>
+	)
+}, 1000);
+
+export default SimpleCounter;
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+//ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
 
